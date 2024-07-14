c85e6b15796c3d0cbd693711bf76d19fbf6f6d5d
Harry-Hunter23 <biohardtomemorize@gmail.com>
2024-07-14 18:11:17 +0530

    projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,88 +1,110 @@
 import React from "react";
-import profile from "../assets/profile_pic_portfolio.jpeg";
-import { FaLinkedinIn, FaGithub, FaWhatsapp, FaTelegram } from "react-icons/fa";
-import resumePath from "../assets/ShrihariD_resume.pdf";
+import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
 
-const Navbar = () => {
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href = resumePath;
-    link.download = "Shrihari_Resume.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  const iconVariants = {
-    hidden: { opacity: 0, x: -20 },
-    visible: (i) => ({
-      opacity: 1,
-      x: 0,
-      transition: {
-        delay: i * 0.2,
-        duration: 0.5,
-      },
-    }),
-  };
-
+const Projects = () => {
   return (
     <>
-      <nav className="max-w-[1200px] mx-auto my-5 flex flex-col md:flex-row items-center justify-between px-4 md:px-0">
-        <div className="w-[65px] h-[65px] rounded-full my-2 p-1 mx-3">
-          <motion.img
-            src={profile}
-            alt=""
-            className="rounded-full mx-2"
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.3 }}
-          />
-        </div>
-        <div className="flex items-center gap-4 md:gap-6 text-xl md:text-2xl mt-4 md:mt-0">
-          {[
-            {
-              href: "https://www.linkedin.com/in/shrihari-deshmukh-6373b9248?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-              icon: <FaLinkedinIn className="cursor-pointer" />,
-            },
-            {
-              href: "https://github.com/Harry-Hunter23",
-              icon: <FaGithub className="cursor-pointer" />,
-            },
-            {
-              href: "https://wa.me/8830984614",
-              icon: <FaWhatsapp className="cursor-pointer" />,
-            },
-            {
-              href: "https://t.me/MrMister888",
-              icon: <FaTelegram className="cursor-pointer" />,
-            },
-          ].map((item, index) => (
-            <motion.a
-              key={index}
-              href={item.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              custom={index}
-              initial="hidden"
-              animate="visible"
-              variants={iconVariants}
-            >
-              {item.icon}
-            </motion.a>
-          ))}
-        </div>
-        <motion.button
-          onClick={handleDownload}
-          className="relative mt-4 md:mt-0 px-6 py-3 font-semibold text-white rounded-lg overflow-hidden bg-gradient-to-r from-purple-400 to-blue-600 shadow-lg group"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.3 }}
+      <div className="border-b border-neutral-900 pb-4">
+        <motion.h2
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.5 }}
+          className="my-20 text-4xl text-center"
         >
-          <span className="absolute inset-0 w-full h-full bg-gradient-to-r from-purple-600 to-blue-800 opacity-0 group-hover:opacity-100 transition duration-300"></span>
-          <span className="relative">Download CV</span>
-        </motion.button>
-      </nav>
+          Projects
+        </motion.h2>
+        <div className="flex flex-wrap justify-center">
+          {PROJECTS.map((project, index) => {
+            return (
+              <div
+                key={index}
+                className="mb-8 flex flex-col items-center lg:flex-row lg:justify-start lg:w-2/3"
+              >
+                <motion.div
+                  whileInView={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, x: -100 }}
+                  transition={{ duration: 0.5 }}
+                  className="aspect-w-1 aspect-h-1 mb-6 w-full max-w-xs lg:mr-8"
+                >
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="object-cover w-full h-full rounded-lg shadow-lg"
+                  />
+                </motion.div>
+                <motion.div
+                  whileInView={{ opacity: 1, x: 0 }}
+                  initial={{ opacity: 0, x: 100 }}
+                  transition={{ duration: 0.5 }}
+                  className="w-full max-w-xl lg:w-auto"
+                >
+                  <h6 className="mb-2 text-xl font-semibold text-gray-200">
+                    {project.title}
+                  </h6>
+                  <p className="mb-4 text-neutral-400">{project.description}</p>
+                  <div className="mb-4">
+                    {project.technologies.map((tech, index) => {
+                      return (
+                        <span
+                          className="mr-2 mb-2 inline-block rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                          key={index}
+                        >
+                          {tech}
+                        </span>
+                      );
+                    })}
+                  </div>
+                  <div className="flex space-x-4">
+                    <a
+                      href={project.liveDemo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="relative inline-block text-lg group"
+                    >
+                      <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out bg-blue-600 rounded-lg group-hover:bg-blue-700">
+                        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gradient-to-br from-purple-400 to-blue-600"></span>
+                        <span className="relative cursor-pointer">
+                          Live Demo
+                        </span>
+                      </span>
+                      <span
+                        className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gradient-to-br from-purple-400 to-blue-600"
+                        style={{
+                          filter: "blur(10px)",
+                          transform: "translate(0, 0)",
+                          transition: "opacity 0.3s ease-in-out",
+                        }}
+                      ></span>
+                    </a>
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="relative inline-block text-lg group"
+                    >
+                      <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-white transition-colors duration-300 ease-out bg-gray-700 rounded-lg group-hover:bg-gray-800">
+                        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gradient-to-br from-purple-400 to-blue-600"></span>
+                        <span className="relative cursor-pointer">GitHub</span>
+                      </span>
+                      <span
+                        className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gradient-to-br from-purple-400 to-blue-600"
+                        style={{
+                          filter: "blur(10px)",
+                          transform: "translate(0, 0)",
+                          transition: "opacity 0.3s ease-in-out",
+                        }}
+                      ></span>
+                    </a>
+                  </div>
+                </motion.div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };
 
-export default Navbar;
+export default Projects;
