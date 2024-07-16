06b48e5af042c8aea8631d60035f4c90abd54017
Harry-Hunter23 <biohardtomemorize@gmail.com>
2024-07-16 18:10:45 +0530

    p5 js is incredible

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,13 @@ import Tech from "./components/Tech";
 import Tools from "./components/Tools";
 import Projects from "./components/Projects";
 import Contacts from "./components/Contacts";
+import BackgroundAnimation from "./components/BackgroundAnimation";
 
 const App = () => {
   return (
     <>
       <div className="overflow-x-hidden antialiased text-cyan-300 selection:text-cyan-900 selection:bg-cyan-300">
-        <div className="fixed top-0 -z-10 h-full w-full">
-          {" "}
-          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-        </div>
-
+        <BackgroundAnimation />
         <div className="container mx-auto px-8">
           <Navbar />
           <Hero />
---
+++ src/components/BackgroundAnimation.jsx
@@ -0,0 +1,93 @@
+import React, { useRef, useEffect } from "react";
+import p5 from "p5";
+
+const BackgroundAnimation = () => {
+  const sketchRef = useRef();
+
+  useEffect(() => {
+    const sketch = (p) => {
+      let particles = [];
+      const numParticles = 200;
+      const repelDistance = 100;
+
+      p.setup = () => {
+        p.createCanvas(window.innerWidth, window.innerHeight);
+        for (let i = 0; i < numParticles; i++) {
+          particles.push(new Particle(p));
+        }
+      };
+
+      p.draw = () => {
+        p.background(0);
+        particles.forEach((particle) => {
+          particle.update();
+          particle.edges();
+          particle.show();
+        });
+      };
+
+      class Particle {
+        constructor(p) {
+          this.p = p;
+          this.pos = p.createVector(p.random(p.width), p.random(p.height));
+          this.vel = p.createVector(p.random(-1, 1), p.random(-1, 1));
+          this.acc = p.createVector(0, 0);
+        }
+
+        update() {
+          let mouse = this.p.createVector(this.p.mouseX, this.p.mouseY);
+          let force = p5.Vector.sub(this.pos, mouse);
+          let distance = force.mag();
+
+          if (distance < repelDistance) {
+            force.setMag(0.5);
+            this.acc.add(force);
+          }
+
+          this.vel.add(this.acc);
+          this.pos.add(this.vel);
+          this.acc.mult(0);
+        }
+
+        edges() {
+          if (this.pos.x > this.p.width) {
+            this.pos.x = this.p.width;
+            this.vel.x *= -1;
+          } else if (this.pos.x < 0) {
+            this.pos.x = 0;
+            this.vel.x *= -1;
+          }
+
+          if (this.pos.y > this.p.height) {
+            this.pos.y = this.p.height;
+            this.vel.y *= -1;
+          } else if (this.pos.y < 0) {
+            this.pos.y = 0;
+            this.vel.y *= -1;
+          }
+        }
+
+        show() {
+          this.p.stroke(255);
+          this.p.strokeWeight(2);
+          this.p.point(this.pos.x, this.pos.y);
+        }
+      }
+    };
+
+    const myP5 = new p5(sketch, sketchRef.current);
+
+    return () => {
+      myP5.remove();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={sketchRef}
+      className="fixed top-0 left-0 w-full h-full -z-10"
+    ></div>
+  );
+};
+
+export default BackgroundAnimation;
